8bc6209110ad3c5a514ca5a857eadae30de13f39
kingpulsar <t.landmeter@gmail.com>
2020-03-31 12:31:38 +0200

    Added Calculator class with sum method and stubs

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public double multiply(int a, int b) {
+
+    }
+
+    public double divide(int a, int b) {
+
+    }
+
+}
